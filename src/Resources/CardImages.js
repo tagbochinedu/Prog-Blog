4b95765b224dff2fb9396bc4f91f06721d8435c2
Tagbo Chinedu <johntagbo2@gmail.com>
2022-07-17 22:27:01 +0100

    final commit

--- src/Resources/CardImages.js
+++ src/Resources/CardImages.js
@@ -3,6 +3,7 @@ import project2 from "./project2.png";
 import project4 from "./project4.png";
 import appStation from "./appStation.png";
 import WebsiteBlog from "./Website-Blog.png";
+import thefoodplace from "./the-food-place.png";
 
 export const images = [
   { name: "project1", url: project1 },
@@ -10,4 +11,5 @@ export const images = [
   { name: "project4", url: project4 },
   { name: "appStation", url: appStation },
   { name: "Website-Blog", url: WebsiteBlog },
+  { name: "the-food-place", url: thefoodplace },
 ];
